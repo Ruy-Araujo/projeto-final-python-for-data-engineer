1e54b36222f29ae7b254399fcaa7891827233e70
Pedro Bassetto <pedrobassetto1997@gmail.com>
2023-08-23 18:16:12 -0300

    Alteração de API OHLC

--- database/models.py
+++ database/models.py
@@ -40,83 +40,43 @@ class AbstractModel():
         return self.cursor.fetchone()
 
 
-class Assets(AbstractModel):
+class HistoricalOHLC(AbstractModel):
     def __init__(self):
-        super().__init__(table_name='assets')
-
-    def create_table(self):
-        self.cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS {self.table_name} (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name VARCHAR(50) NOT NULL UNIQUE,
-                symbol VARCHAR(5) NOT NULL,
-                insert_at TIMESTAMP
-            );
-        """)
-
-    def insert(self, item):
-        self.cursor.execute(f"""
-            INSERT OR REPLACE INTO {self.table_name}
-            VALUES (null, ?, ?, '{datetime.now()}');
-        """, item)
-        self.conn.commit()
-
-
-class AssetsHistory(AbstractModel):
-    def __init__(self):
-        super().__init__(table_name='assets_history')
+        super().__init__(table_name='ohlc_historical')
 
     def create_table(self):
         self.cursor.execute(f"""
             CREATE TABLE IF NOT EXISTS {self.table_name} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 date DATE NOT NULL,
-                price REAL NOT NULL,
-                asset_id INTEGER NOT NULL,
-                insert_at TIMESTAMP,
-                FOREIGN KEY (asset_id) REFERENCES assets (id)
+                price_open REAL NOT NULL, 
+                price_high REAL NOT NULL, 
+                price_low REAL NOT NULL, 
+                price_close REAL NOT NULL, 
+                insert_at TIMESTAMP            
             );
         """)
 
     def insert(self, item):
         self.cursor.execute(f"""
             INSERT OR REPLACE INTO {self.table_name}
-            VALUES (null, ?, ?, ?, '{datetime.now()}');
+            VALUES (null, ?, ?, ?, ?, ?, '{datetime.now()}');
         """, item)
         self.conn.commit()
 
 
 if __name__ == '__main__':
-    # Execute este arquivo para criar o banco de dados e a tabela
-    assets = Assets()
-    assets.drop_table()
-    assets.create_table()
+
+    historical = HistoricalOHLC()
+    historical.drop_table()
+    historical.create_table()
     items = [
-        ('Bitcoin', 'BTC'),
-        ('Ethereum', 'ETH'),
-        ('Tether', 'USDT'),
+        ('2021-01-01', 1000, 1000, 1000, 1000),
+        ('2021-01-02', 2000, 2000, 2000, 2000),
+        ('2021-01-03', 3000, 3000, 3000, 3000)
     ]
 
     for item in items:
-        assets.insert(item)
+        historical.insert(item)
 
-    print(assets.read_all())
-
-    assets_history = AssetsHistory()
-    assets_history.drop_table()
-    assets_history.create_table()
-    items = [
-        ('2021-01-01', 1000, 1),
-        ('2021-01-02', 2000, 1),
-        ('2021-01-03', 3000, 1),
-        ('2021-01-01', 100, 2),
-        ('2021-01-02', 200, 2),
-        ('2021-01-03', 300, 2),
-        ('2021-01-01', 1, 3),
-        ('2021-01-02', 2, 3),
-        ('2021-01-03', 3, 3),
-    ]
-
-    for item in items:
-        assets_history.insert(item)
-    print(assets_history.read_all())
+    print(historical.read_all())
