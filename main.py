8769326f577f596775fe68e534d42d054e0503ed
Ruy Araujo <raraujo@vibra.digital>
2023-08-23 23:44:01 -0300

    feat: Add argumentos de execução

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from app.scraper import Scraper
 from database.models import HistoricalOHLC
 import asyncio
+import argparse
+
+
+async def parse_args():
+    parser = argparse.ArgumentParser(description='Scraper de dados do CoinMarketCap')
+    parser.add_argument('-id', '--coin-id', type=int, help='Coin ID do CoinMarketCap')
+    parser.add_argument('-start', '--date-start', type=str, help='Data de inicio da extração')
+    parser.add_argument('-end', '--date-end', type=str, help='Data de fim da extração')
+    parser.add_argument('-i', '--interval', type=str, help='Frequencia de extração (1d, 1h, 1m)')
+    parser.add_argument('-c', '--convert', type=str, help='Moeda de conversão (BRL, USD, EUR)')
+    return parser.parse_args()
 
 
 async def main():
-    scraper = Scraper(coin_id=1, date_start='2021-01-01', date_end='2021-01-31', interval='1d', convert='BRL')
+    args = await parse_args()
+    scraper = Scraper(
+        coin_id=args.coin_id,
+        date_start=args.date_start,
+        date_end=args.date_end,
+        interval=args.interval,
+        convert=args.convert)
     results = await scraper.scrap_historical()
-    # await scraper.close()
 
     db = HistoricalOHLC()
     await db.create_table()
