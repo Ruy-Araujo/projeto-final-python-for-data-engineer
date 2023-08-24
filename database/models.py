84a03d89401d78d040ebfbd4c3ee3366e8e569da
Ruy Araujo <raraujo@vibra.digital>
2023-08-23 23:17:03 -0300

    refactor/break!: Suporte a metodos async no database

--- database/models.py
+++ database/models.py
@@ -3,50 +3,61 @@
 # realizar operações básicas de criação de tabelas, inserção de dados e consultas. O código define uma
 # classe chamada "Currency" que encapsula as operações relacionadas à tabela "currency" em um banco de
 # dados chamado "cryptoCurrency.sqlite".
-
 import sqlite3
 from datetime import datetime
 
-db_name = 'coin_cap.sqlite'
+db_name = 'database.sqlite'
 
 
 class AbstractModel():
     def __init__(self, table_name):
         self.table_name = table_name
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
 
-    def drop_table(self):
-        self.cursor.execute(f"""
+    async def execute_query(self, query, parameters=None):
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        if parameters:
+            cursor.execute(query, parameters)
+        else:
+            cursor.execute(query)
+
+        result = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        return result
+
+    async def drop_table(self):
+        await self.cursor.execute(f"""
             DROP TABLE IF EXISTS {self.table_name};
         """)
 
-    def read_all(self):
-        self.cursor.execute(f"""
+    async def read_all(self):
+        await self.cursor.execute(f"""
             SELECT * FROM {self.table_name};
         """)
         return self.cursor.fetchall()
 
-    def read_many(self, limit):
-        self.cursor.execute(f"""
+    async def read_many(self, limit):
+        await self.cursor.execute(f"""
             SELECT * FROM {self.table_name} LIMIT {limit};
         """)
         return self.cursor.fetchall()
 
-    def read_one(self, id):
-        self.cursor.execute(f"""
+    async def read_one(self, id):
+        await self.cursor.execute(f"""
             SELECT * FROM {self.table_name} WHERE id = {id};
         """)
         return self.cursor.fetchone()
-    
-    def read_count(self):
-        self.cursor.execute(f"""
+
+    async def read_count(self):
+        await self.cursor.execute(f"""
             SELECT count(1) registros FROM {self.table_name};
         """)
         return self.cursor.fetchone()
-    
-    def read_with_filter(self, filter):
-        self.cursor.execute(f"""
+
+    async def read_with_filter(self, filter):
+        await self.cursor.execute(f"""
             SELECT * FROM {self.table_name} WHERE {filter};
         """)
         return self.cursor.fetchall()
@@ -56,39 +67,23 @@ class HistoricalOHLC(AbstractModel):
     def __init__(self):
         super().__init__(table_name='ohlc_historical')
 
-    def create_table(self):
-        self.cursor.execute(f"""
+    async def create_table(self):
+        await self.execute_query(f"""
             CREATE TABLE IF NOT EXISTS {self.table_name} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(100) NOT NULL,
+                symbol VARCHAR(10) NOT NULL,
                 date DATE NOT NULL,
-                price_open REAL NOT NULL, 
-                price_high REAL NOT NULL, 
-                price_low REAL NOT NULL, 
-                price_close REAL NOT NULL, 
-                insert_at TIMESTAMP            
+                price_open REAL NOT NULL,
+                price_high REAL NOT NULL,
+                price_low REAL NOT NULL,
+                price_close REAL NOT NULL,
+                insert_at TIMESTAMP
             );
         """)
 
-    def insert(self, item):
-        self.cursor.execute(f"""
+    async def insert(self, item):
+        await self.execute_query(f"""
             INSERT OR REPLACE INTO {self.table_name}
-            VALUES (null, ?, ?, ?, ?, ?, '{datetime.now()}');
+            VALUES (null, ?, ?, ?, ?, ?, ?, ?, '{datetime.now()}');
         """, item)
-        self.conn.commit()
-
-
-if __name__ == '__main__':
-
-    historical = HistoricalOHLC()
-    historical.drop_table()
-    historical.create_table()
-    items = [
-        ('2021-01-01', 1000, 1000, 1000, 1000),
-        ('2021-01-02', 2000, 2000, 2000, 2000),
-        ('2021-01-03', 3000, 3000, 3000, 3000)
-    ]
-
-    for item in items:
-        historical.insert(item)
-
-    print(historical.read_all())
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app.scraper import Scraper
+from database.models import HistoricalOHLC
 import asyncio
 
 
@@ -6,7 +7,25 @@ async def main():
     scraper = Scraper(coin_id=1, date_start='2021-01-01', date_end='2021-01-31', interval='1d', convert='BRL')
     results = await scraper.scrap_historical()
     # await scraper.close()
-    print(results)
+
+    db = HistoricalOHLC()
+    await db.create_table()
+
+    for result in results:
+        coin_info = (result['data']['name'], result['data']['symbol'])
+
+        for quote in result['data']['quotes']:
+            data = (
+                *coin_info,
+                quote['quote']['timestamp'][:10],
+                quote['quote']['open'],
+                quote['quote']['high'],
+                quote['quote']['low'],
+                quote['quote']['close']
+            )
+
+            await db.insert(data)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
